c06644ade1384f7edecc0c97a87257ccefb94d29
Michael Wang <wangqiangshen@gmail.com>
2020-07-09 16:17:14 +0800

    update cascader

--- src/components/cascader/VCascader.vue
+++ src/components/cascader/VCascader.vue
@@ -1,48 +1,24 @@
 <template>
   <div class="v-cascader d-flex">
-    <template v-for="(node, key) in nodes">
-      <v-cascader-list
-        class="flex"
-        :items="node"
-        :key="key"
-        @selected="handleSelectItem"
-        v-show="showList(node)"
-      />
-    </template>
+    <v-cascader-list :items="items" />
   </div>
 </template>
 <script>
 import VCascaderList from './VCascaderList.vue'
-import flattern from '@/utils/flattern'
-import _groupBy from 'lodash/groupBy'
 export default {
   name: 'VCascader',
   props: {
     items: Array
   },
   components: { VCascaderList },
-  computed: {
-    nodes() {
-      const options = {
-        initNode: (node) => node // <= default, consider node => _.clone(node) to avoid mutating the tree
-      }
-      return _groupBy(flattern(this.items, options), 'depth')
-    }
-  },
+  computed: {},
   data() {
     return {
-      selectedItem: null,
       selectedItems: []
     }
   },
   methods: {
-    handleSelectItem(item) {
-      this.selectedItems.push(item)
-    },
-    showList(node) {
-      // const item = node[0]
-      return true
-    }
+    handleSelectItem(item) {}
   },
   created() {}
 }
--- src/components/cascader/VCascaderList.vue
+++ src/components/cascader/VCascaderList.vue
@@ -1,44 +1,48 @@
 <template>
-  <v-list class="v-cascader__list" tag="ul" dense>
-    <v-list-item-group v-model="selectedItem" @change="handleChange">
-      <template v-for="(item, key) in items">
-        <v-list-item
-          tag="li"
-          :key="key"
-          :value="item"
-          v-show="showItem(item)"
-          :class="'child_of_' + item.parent_id"
-        >
-          <v-list-item-title>{{ item.name }}</v-list-item-title>
-        </v-list-item>
-      </template>
-    </v-list-item-group>
-  </v-list>
+  <div class="d-flex">
+    <v-list
+      class="v-cascader__list pa-0 flex"
+      style="border-right: 1px solid #eee"
+      tag="ul"
+      dense
+    >
+      <v-list-item-group v-model="selectedItem" @change="handleChange">
+        <template v-for="item in items">
+          <v-list-item tag="li" :key="item.id" :value="item">
+            <v-list-item-title>{{ item.name }}</v-list-item-title>
+            <v-list-item-icon v-if="item.children && item.children.length > 0">
+              <v-icon>mdi-chevron-down</v-icon>
+            </v-list-item-icon>
+          </v-list-item>
+        </template>
+      </v-list-item-group>
+    </v-list>
+    <template v-if="selectedItem && selectedItem.children">
+      <v-cascader-list class="flex" :items="selectedItem.children" />
+    </template>
+  </div>
 </template>
 <script>
+import VCascaderList from './VCascaderList'
 export default {
   name: 'VCascaderList',
   props: {
     items: Array
   },
-  components: {},
+  components: { VCascaderList },
   data() {
     return {
-      selectedItem: null,
-      selectedItems: []
+      selectedItem: null
     }
   },
-
   methods: {
-    showItem(item) {
-      return item.depth === 1
-    },
-    handleChange(item) {
-      const selector = '.child_of_' + item.id
-      document.querySelectorAll(selector).forEach((el) => {
-        el.style.display = 'flex'
-      })
-    }
+    handleChange() {}
   }
 }
 </script>
+
+<style lang="scss">
+.cascader__list {
+  border-right: 1px solid #000;
+}
+</style>
